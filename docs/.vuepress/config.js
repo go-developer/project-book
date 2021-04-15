6645998aca8938bbb294a08f7aaacee2bb305f8d
白茶清欢 <go_developer@163.com>
2021-04-15 22:43:10 +0800

    增加 code demo插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
     editLinks: false,
   },
   plugins: [
-    [
+    [ // 基于github issue的评论插件
       'gitalk-maker',
       {
         gitalkConfig: {
@@ -32,5 +32,33 @@ module.exports = {
         },
       },
     ],
+    [ // 代码样式插件
+      'demo-code',
+      {
+        jsLibs: [
+          // umd
+          'https://unpkg.com/tua-storage/dist/TuaStorage.umd.js',
+        ],
+        cssLibs: [
+          'https://unpkg.com/animate.css@3.7.0/animate.min.css',
+        ],
+        showText: 'show code',
+        hideText: 'hide',
+        styleStr: 'text-decoration: underline;',
+        minHeight: 200,
+        onlineBtns: {
+          codepen: true,
+          jsfiddle: true,
+          codesandbox: true,
+        },
+        codesandbox: {
+          deps: { 'lodash': 'latest' },
+          json: '',
+          query: '',
+          embed: '',
+        },
+        demoCodeMark: 'demo-code',
+        copyOptions: { align: 'top', selector: '.demo-and-code-wrapper div[class*="language-"] pre' },
+      }]
   ],
 };
